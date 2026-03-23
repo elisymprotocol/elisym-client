db4f41a5a5d4c1c32c7432257044e2226375f166
Igor Peregudov <igor.peregudov93@gmail.com>
2026-03-23 18:58:21 +0000

    wip

--- src/constants.rs
+++ src/constants.rs
@@ -3,4 +3,4 @@
 pub const ELISYM_PROTOCOL_PUBKEY: &str = "873bcdb9ec10f277d543ffcf3b49b2cbbab5392c99abf34b720af29e61c43b98";
 
 /// Extra relays to connect for auto-engage (elisymlabs posts live here).
-pub const ENGAGE_RELAYS: &[&str] = &["wss://relay.primal.net"];
+pub const ENGAGE_RELAYS: &[&str] = &["wss://relay.primal.net", "wss://purplepag.es"];
